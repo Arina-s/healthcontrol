ec19df661e319f219372f68b09a1684e5aa3d804
Arina Slizh <arina_slizh@epam.com>
2021-07-24 09:53:24 +0300

    Refactoring

--- src/main/java/com/arinauniversity/healthcontrol/config/DatabaseConfig.java
+++ src/main/java/com/arinauniversity/healthcontrol/config/DatabaseConfig.java
@@ -1,4 +1,4 @@
-package com.arinauniversity.healthcontrol;
+package com.arinauniversity.healthcontrol.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
